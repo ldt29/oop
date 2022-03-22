114127110c02031bb0d52e26531f1cbba3cc3fbc
ldt29 <72798752+ldt29@users.noreply.github.com>
2022-03-22 14:53:51 +0800

    Add files via upload

---
+++ hw2-C/Map.cpp
@@ -0,0 +1,35 @@
+#include "Map.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+Map::Map(int n){
+    data = new Pair[n];
+    sz=0;
+}
+
+int & Map::operator[](string name){
+    for(int i=0;i<sz;i++){
+        if(data[i].hasKey(name)){
+            return data[i].getVal();
+        }
+    }
+    data[sz].reset(name,0);
+    return data[sz++].getVal();
+}
+const int & Map::operator[](string name) const{
+    for(int i=0;i<sz;i++){
+        if(data[i].hasKey(name)){
+            return data[i].getVal();
+        }
+    }
+    return data[sz].getVal();
+}
+
+int Map::size(){
+    return sz;
+}
+
+Map::~Map(){
+    if(data) delete[] data;
+}
---
+++ hw2-C/Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Pair.h"
+#include <string>
+
+class Map{
+    Pair * data;
+    int sz;
+public:
+    Map(int n);
+    // TODO
+    int & operator [](string name);
+    const int & operator[](string name)const;
+    int size();
+    ~Map();
+};
